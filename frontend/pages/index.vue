6bb962fdee7461203e4ca082d35210284edf53ef
ZhangTerrence <71456929+ZhangTerrence@users.noreply.github.com>
2021-12-17 12:27:20 -0500

    Save

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <Tutorial />
+  <section id="app">
+    <h1>{{count}}</h1>
+    <button @click="add"></button>
+  </section>
 </template>
 
 <script>
 export default {
+  el: "#app",
   name: 'IndexPage',
+  data(){
+    return{
+      count: 0
+    }
+  },
+  methods: {
+    add: function(){
+      return this.data.count++
+    }
+  }
 }
 </script>
+
+<style>
+button{
+  width: 10rem;
+}
+</style>

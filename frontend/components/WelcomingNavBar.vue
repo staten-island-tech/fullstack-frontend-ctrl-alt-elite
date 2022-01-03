d2296a7c02f1d0f2474d77cbba8cff259c6865f4
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-01-03 12:18:35 -0500

    Update WelcomingNavBar.vue

--- frontend/components/WelcomingNavBar.vue
+++ frontend/components/WelcomingNavBar.vue
@@ -1,6 +1,10 @@
 <template>
-  <section id="welcoming-nav">
-     <img class="logo" src="../assets/logo-placeholder.png"> 
+  <section class="border-b-2 flex flex-row justify-between items-center">
+     <img class="h-12 mx-4" src="../assets/logo-placeholder.png"> 
+     <div class="items-center mx-5">
+         <bottom><NuxtLink to="/SignUp" class="text-base text-white mx-3 p-2">Sign Up</NuxtLink></bottom>
+         <button><NuxtLink to="/Login" class="text-base text-white border-2 rounded p-2">Login</NuxtLink></button>
+     </div>
   </section>
 </template>
 
@@ -11,12 +15,5 @@ export default {
 </script>
 
 <style scoped>
-    #welcoming-nav {
-        height: 7vh;
-        border-bottom: white 1px solid;
-    }
 
-    .logo {
-        height: 3rem;
-    }
 </style>

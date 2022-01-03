3c12092724a93742302424357a74e6d89e50f7ff
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-01-03 12:26:25 -0500

    Update WelcomingNavBar.vue

--- frontend/components/WelcomingNavBar.vue
+++ frontend/components/WelcomingNavBar.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="border-b-2 flex flex-row justify-between items-center">
+  <section class="flex flex-row justify-between items-center">
      <img class="h-12 mx-4" src="../assets/logo-placeholder.png"> 
      <div class="items-center mx-5">
          <bottom><NuxtLink to="/SignUp" class="text-base text-white mx-3 p-2">Sign Up</NuxtLink></bottom>
-         <button><NuxtLink to="/Login" class="text-base text-white border-2 rounded p-2">Login</NuxtLink></button>
+         <button><NuxtLink to="/Login" class="text-base text-white border-2 h-10 px-6 rounded-md border py-1">Login</NuxtLink></button>
      </div>
   </section>
 </template>

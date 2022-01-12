ef45c7fd1cc7b768a833ac219441d9feff3f9137
Rafia7 <71458610+Rafia7@users.noreply.github.com>
2022-01-12 12:27:57 -0500

    2

---
+++ frontend/pages/ProjectsPage.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="temp1">
+    <div class="menu">
+        Menu
+    </div>
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.temp1{
+    background-color: aquamarine;
+    opacity: 85%;
+    font-family: 'Courier New', Courier, monospace;
+    color: black;
+    font-size: 3rem;
+    text-align: left;
+    
+};
+.menu{
+    font-weight: bold;
+    text-align: left;
+    border-radius: 2rem;
+    background-color: white;
+}
+menu:hover{
+    transform: scale(1.1);
+    cursor: pointer;
+}
+
+</style>

866e981538632078dad74c419f5c431dc29a4b27
jysleyin <69206877+jysleyin@users.noreply.github.com>
2022-01-13 12:08:10 -0500

    Update DefaultNavBar.vue

---
+++ frontend/components/DefaultNavBar.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <section class="flex flex-row justify-between items-center">
+    <img class="h-12 mx-4" src="../assets/logo-placeholder.png"> 
+       <LogoutButton/>
+  </section>
+
+</template>
+    
+<script>
+export default {
+
+}
+</script>

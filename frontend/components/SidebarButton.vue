88e5c56f5c3bfcad551a0827dccfefd6e6a06f60
jysleyin <69206877+jysleyin@users.noreply.github.com>
2022-01-13 12:26:58 -0500

    Create SidebarButton.vue

---
+++ frontend/components/SidebarButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-indigo-800 hidden">
+        <button onclick="openSide()">Close</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped> 
+</style>

32e4bf287e4cfd40854d233c6eb04a5943f3dfac
jysleyin <69206877+jysleyin@users.noreply.github.com>
2022-01-14 12:26:35 -0500

    Update SidebarButton.vue

--- frontend/components/SidebarButton.vue
+++ frontend/components/SidebarButton.vue
@@ -1,14 +1,29 @@
 <template>
-    <div class="bg-indigo-800 hidden">
-        <button onclick="openSide()">Close</button>
+<div class="container">
 
+    <div id="mySidebar" class="bg-indigo-800 hidden">
+        <button onclick="closeSide()">Close</button>
     </div>
+    <div>
+        <button class="text-neutral-50" onclick="openSide()">Open</button>
+    </div>
+</div>
+     
 </template>
 
 <script>
 export default {
-    
-}
+
+       methods: {
+         openSide:  function openSide() {
+               document.getElementById("mySideBar").style.display = "block";
+       },
+        closeSide: function closeSide() {
+            document.getElementById("mySideBar").style.display = "none";
+        },
+},
+};
+
 </script>
 
 <style scoped> 

1fa582d6a2931446bdc963bf6064f74ef6328599
ZhangTerrence <71456929+TerrenceZhang74@users.noreply.github.com>
2022-01-18 11:55:04 -0500

    E

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,15 +1,3 @@
-export const state = () => ({
-  counter: 3,
-})
-export const mutations = {
-  INCREMENT_COUNT(state, incrementBy) {
-    state.counter += incrementBy
-  },
-}
-export const actions = {
-  updateCount({ state, commit }, incrementBy) {
-    if (state.user) {
-      commit('INCREMENT_COUNT', incrementBy)
-    }
-  },
-}
+export const state = () => ({})
+export const mutations = {}
+export const actions = {}

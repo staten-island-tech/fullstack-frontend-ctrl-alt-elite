7ca908361ca6138e62b0a809f8cb027608b5eb34
ZhangTerrence <71456929+TerrenceZhang74@users.noreply.github.com>
2022-01-21 11:56:25 -0500

    Before

--- frontend/components/ProjectsNavBar.vue
+++ frontend/components/ProjectsNavBar.vue
@@ -1,9 +1,18 @@
 <template>
-  <nav class="w-full h-1/10 bg-black"></nav>
+  <nav class="w-full h-1/10 p-4 bg-gray-500">
+    <button class="float-right" @click="updateFrame">Run</button>
+  </nav>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    updateFrame(){
+      const htmlCode = document.getElementById('html').value
+      // const cssCode = document.getElementById('css').value
+      const iframe = document.getElementById('frame').contentWindow.document
+      iframe.src = "data:text/html;charset=utf-8," + encodeURI(htmlCode)
+    }
+  }
 }
 </script>
--- frontend/pages/Project.vue
+++ frontend/pages/Project.vue
@@ -1,12 +1,23 @@
 <template>
   <section class="h-screen w-screen">
     <projectsNavBar/>
-    <div class="h-2/5 w-full flex row">
-        <div contenteditable="true" class="h-full w-1/3 p-4 align-top bg-gray-500"></div>
-        <div contenteditable="true" class="h-full w-1/3 p-4 bg-red-500"></div>
-        <div contenteditable="true" class="h-full w-1/3 p-4 bg-yellow-500"></div>
+    <div id="edit-container" class="h-2/5 w-full flex row">
+        <div class="h-full w-1/3 p-4 bg-indigo-500">
+          <span class="h-1/10 w-full">HTML</span>
+          <textarea id="html" class="h-9/10 w-full bg-transparent"></textarea>
+        </div>
+        <div class="h-full w-1/3 p-4 bg-red-500">
+          <span class="h-1/10 w-full">CSS</span>
+          <textarea id="css" class="h-9/10 w-full bg-transparent"></textarea>
+        </div>
+        <div class="h-full w-1/3 p-4 bg-yellow-500">
+          <span class="h-1/10 w-full">Javascript</span>
+          <textarea id="js" class="h-9/10 w-full bg-transparent"></textarea>
+        </div>
     </div>
-    <iframe class="h-3/5 w-full bg-green-500"></iframe>
+    <div id="output-container" class="h-1/2 w-full">
+      <iframe id='frame' class="h-full w-full bg-green-500"></iframe>
+    </div>
   </section>
 </template>
 
@@ -15,6 +26,8 @@ import projectsNavBar from "../components/ProjectsNavBar.vue"
 export default {
     components:{
         projectsNavBar
+    },
+    methods:{
     }
 }
 </script>

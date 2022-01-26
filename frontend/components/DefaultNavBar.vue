a1cd8b7dcc359ea05df68a68c202bca67427d8df
jysleyin <69206877+jysleyin@users.noreply.github.com>
2022-01-26 12:10:56 -0500

    Update DefaultNavBar.vue

--- frontend/components/DefaultNavBar.vue
+++ frontend/components/DefaultNavBar.vue
@@ -2,9 +2,12 @@
 
 
   <section class="flex flex-row flex-start items-center">
+    <div class="flex flex-start">
       <SidebarButton/>
     <img class="h-12 mx-4" src="../assets/logo-placeholder.png"> 
-      
+     
+       </div>
+        <NewProjectButton />
   </section>
   
   

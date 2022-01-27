539e05a5cd11fdd2686d3da1c98f3485cea81ee4
ZhangTerrence <71456929+TerrenceZhang74@users.noreply.github.com>
2022-01-27 12:26:34 -0500

    Trying Ace

--- frontend/components/ProjectsNavBar.vue
+++ frontend/components/ProjectsNavBar.vue
@@ -1,17 +1,18 @@
 <template>
   <nav class="w-full h-1/10 p-4 bg-gray-500">
-    <button class="float-right" @click="updateFrame">Run</button>
+    <button class="float-right" @click="run">Run</button>
   </nav>
 </template>
 
 <script>
+import AceEditor from "vue2-ace-editor";
 export default {
+  components:{
+    AceEditor
+  },
   methods:{
-    updateFrame(){
-      const htmlCode = document.getElementById('html').value
-      // const cssCode = document.getElementById('css').value
-      const iframe = document.getElementById('frame').contentWindow.document
-      iframe.src = "data:text/html;charset=utf-8," + encodeURI(htmlCode)
+    run(){
+      console.log(AceEditor);
     }
   }
 }
--- frontend/pages/Project.vue
+++ frontend/pages/Project.vue
@@ -2,32 +2,47 @@
   <section class="h-screen w-screen">
     <projectsNavBar/>
     <div id="edit-container" class="h-2/5 w-full flex row">
-        <div class="h-full w-1/3 p-4 bg-indigo-500">
-          <span class="h-1/10 w-full">HTML</span>
-          <textarea id="html" class="h-9/10 w-full bg-transparent"></textarea>
+        <div class="h-full w-1/3">
+          <AceEditor v-model="contentHTML" lang="html" theme="twilight" @init="editorInit"></AceEditor>
         </div>
-        <div class="h-full w-1/3 p-4 bg-red-500">
-          <span class="h-1/10 w-full">CSS</span>
-          <textarea id="css" class="h-9/10 w-full bg-transparent"></textarea>
+        <div class="h-full w-1/3">
+          <AceEditor v-model="contentCSS" lang="css" theme="twilight" @init="editorInit"></AceEditor>
         </div>
-        <div class="h-full w-1/3 p-4 bg-yellow-500">
-          <span class="h-1/10 w-full">Javascript</span>
-          <textarea id="js" class="h-9/10 w-full bg-transparent"></textarea>
+        <div class="h-full w-1/3">
+          <AceEditor v-model="contentJS" lang="javascript" theme="twilight" @init="editorInit"></AceEditor>
         </div>
     </div>
-    <div id="output-container" class="h-1/2 w-full">
-      <iframe id='frame' class="h-full w-full bg-green-500"></iframe>
-    </div>
+    <iframe id='frame' class="h-1/2 w-full bg-green-500"></iframe>
   </section>
 </template>
 
 <script>
+import AceEditor from "vue2-ace-editor";
 import projectsNavBar from "../components/ProjectsNavBar.vue" 
+
 export default {
     components:{
-        projectsNavBar
+        projectsNavBar,
+        AceEditor
+    },
+    data(){
+      return{
+        contentHTML:"",
+        contentCSS:"",
+        contentJS:""
+      }
     },
     methods:{
+      editorInit(editor){
+        require('brace/mode/html')                
+        require('brace/mode/javascript') 
+        require('brace/mode/css') 
+        require('brace/theme/twilight')
+        editor.renderer.setShowGutter(true)
+        editor.setOptions({
+          fontSize: '1.5rem',
+        })
+      },
     }
 }
 </script>

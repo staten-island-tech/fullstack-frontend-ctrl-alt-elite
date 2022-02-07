b7da7b039364ee7e21a74c1e47f164e585defa82
cseifen123 <caroline.seifen22@sitechhs.com>
2022-02-07 12:27:45 -0500

    components

---
+++ frontend/pages/EditProfile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <DefaultNavBar/>
+    </div>
+    <div class="bg-red-700 h-28 text-black">
+    </div>
+   
+   
+</template>
+
+
+<script>
+
+import DefaultNavBar  from "../components/DefaultNavBar.vue"
+
+export default {
+    components: {
+        DefaultNavBar,
+    }
+}
+
+</script>

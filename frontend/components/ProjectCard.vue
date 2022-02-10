e265c6c58a13df758f36fdc620b5f2b032e5a46c
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-02-10 12:26:51 -0500

    -

--- frontend/components/ProjectCard.vue
+++ frontend/components/ProjectCard.vue
@@ -3,11 +3,13 @@
     <div class="h-3/4 w-full bg-gray-900 rounded flex justify-center items-center">
       <p class="text-gray-200">sample</p>
     </div>
-    <div class="h-1/4 w-full flex flex-row items-center justify-center">
-      <div class="w-1/6">
+    <div class="h-1/4 w-full flex flex-col justify-center">
+      <h3 class="text-white w-full z-10 flex justify-center px-3 font-bold"><slot>Sample Title</slot></h3>
+      <div class="flex flex-row justify-between px-3">
         <LikeButton/>
+        <h4 class="text-gray-100"><slot>Username</slot></h4>
+        <FollowButton/>
       </div>
-      <h3 class="text-white"><slot>Sample Title</slot></h3>
     </div>
   </div>
 </template>

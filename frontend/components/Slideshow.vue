4b97a48975e8d3f9816cd6c9d080e2a2c7c441d3
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-02-18 12:27:02 -0500

    Update Slideshow.vue

--- frontend/components/Slideshow.vue
+++ frontend/components/Slideshow.vue
@@ -1,28 +1,22 @@
 <template>
-    <!-- <div class="swiper-button-prev"></div> -->
-    <div class="swiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <ProjectCard class="slider-content"/>
-          <ProjectCard class="slider-content"/>
-          <ProjectCard class="slider-content"/>
-          <ProjectCard class="slider-content"/>
-          <ProjectCard class="slider-content"/>
-          <ProjectCard class="slider-content"/>
-        </div>
-        <!-- <div v-for="i in 6" :key="i" class="swiper-slide" :class="`slide--${i}`">
-          <div class="slider-content">Slide {{ i }}</div>
-        </div> -->
+  <div class="swiper">
+    <div class="swiper-wrapper">
+      <div v-for="i in 6" :key="i" class="swiper-slide" :class="`slide--${i}`">
+        <ProjectCard class="slider-content w-full"/>
       </div>
-    
-      <!-- <div class="swiper-pagination"></div> -->
     </div>
-    <!-- <div class="swiper-button-next"></div> -->
+    <!-- If pagination is needed -->
+    <div class="swiper-pagination"></div>
+
+    <!-- If navigation buttons are needed -->
+    <!-- <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div> -->
+  </div>
 </template>
 
 <script>
 // Import Swiper Vue.js components
-import { Swiper,FreeMode, Navigation, Autoplay } from 'swiper'
+import { Swiper,FreeMode, Autoplay } from 'swiper'
 import 'swiper/swiper-bundle.min.css'
 
 export default {
@@ -30,7 +24,7 @@ export default {
     // configure Swiper to use modules. The modules were tested with SwiperJS v6.8.4 with NuxtJS v2.15.7
     // previously it was before export default. Moved here for performance issues. Move back in case of problems.
     // add or remove unused modules
-    Swiper.use([ Navigation, FreeMode, Autoplay])
+    Swiper.use([ FreeMode, Autoplay])
     
     // init Swiper:
     /* eslint-disable no-unused-vars */
@@ -39,8 +33,12 @@ export default {
       // @see https://swiperjs.com/swiper-api#parameters
       direction: 'horizontal',
       freeMode: true,
+      slidesPerView: 3,
+      spaceBetween: 20,
+      // spaceBetween: "30",
+
       // remove unused modules if needed
-      modules: [Navigation, FreeMode, Autoplay],
+      modules: [ FreeMode, Autoplay],
       // Pagination if needed
       // pagination: {
       //   el: '.swiper-pagination',
@@ -64,36 +62,15 @@ export default {
 
 <style scoped>
 .swiper {
-  height: 300px;
+  height: 20rem;
   overflow: hidden;
   position: relative;
-  width: 90vw;
+  width: 80vw;
 }
 .swiper-slide {
   align-items: center;
   display: flex;
   justify-content: center;
-  width: 50%;
 }
-.slider-content {
-  color: #000;
-}
-.slide--1 {
-  background-color: #f1c40f;
-}
-.slide--2 {
-  background-color: #e67e22;
-}
-.slide--3 {
-  background-color: #e74c3c;
-}
-.slide--4 {
-  background-color: #9b59b6;
-}
-.slide--5 {
-  background-color: #3498db;
-}
-.slide--6 {
-  background-color: #2ecc71;
-}
+
 </style>

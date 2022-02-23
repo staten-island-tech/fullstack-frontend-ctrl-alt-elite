9eb7084358aad9d7a09f6853b4d27efc4f60e184
cseifen123 <57498875+cseifen123@users.noreply.github.com>
2022-02-23 00:54:01 -0500

    profile layout

--- frontend/pages/EditProfile.vue
+++ frontend/pages/EditProfile.vue
@@ -1,25 +1,31 @@
 <template>
   <div>
      <DefaultNavBar />
-      <section>
+      <section class=" grid grid-cols-2 gap-4 m-5  ">
         <!-- <button @click="getProfile()">TEST</button>
         <textarea id="" :value="abc"  name=""  cols="30" rows="10"></textarea> -->
-      
-          <div class=" border-white border-2 h-15 w-80 left-2 justify-center">
-             <h1 class="text-left">
-               <input v-model= "message" class="text-black"  placeholder="Edit Name">
-                  <h1 class="text-white">Name: {{ userProfile.data.name }}</h1>
-              </h1>
-              <p class="text-left">Description:
-                <input type="text" class="text-black"> {{userProfile.data.description}}
-              </p>
+          <img class="basis-5 rounded-full h-60 justify-self-center self-center p-0" :src="userProfile.data.profile_pic">
+
+          <div class="grid grid-rows-3  ">
+                <h1 class="h-10"> Name: <input v-model="userProfile.data.name" type="text" class="text-black"  ></h1> 
+                <p>Description: <input v-model="userProfile.data.description" type="text" class="text-black"></p>
+              <div class="grid grid-rows-1 grid-cols-2 gap-4">
+                <div class="border-2 border-gray-50 self-end align-middle text-center" >Followers: 10</div>
+                <div class="border-2  border-gray-50 self-end align-middle text-center">Following: {{ following }} </div>
+              </div>
+             
+            <button @click="updateProfile"> Save</button>
+            <button @click="getProfile"> Reset </button>
+
           </div>
-          <div class="flex h-36 justify-evenly ">
-            <img class="rounded-full" :src="userProfile.data.profile_pic">
-            <div class="border-2 h-20 w-60 border-gray-50 self-end align-middle text-center" >Followers: 10</div>
-            <div class="border-2 h-20 w-60 border-gray-50 self-end align-middle text-center">Following: 15</div>
-          </div>
-          <!-- <h1 class="text-white">hello {{ $auth.user }}</h1> -->
+      </section>
+     <div class="grid grid-cols-1 divide-y-[3px] divide-yellow-500 text-center">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+      <section>
+
       </section>
   </div>
 </template>
@@ -32,12 +38,14 @@ export default {
    data(){
        return{ 
          userProfile: { data : 'abc'},
-  
+        following: 0
          }
       },
-  
+
+   
    mounted ()
    { this.getProfile()} ,   
+    
     
 
   methods: {
@@ -52,15 +60,38 @@ async getProfile() {
         // console.log ("I am here");
         const data = await response.json()
 
-        this.userProfile.data = data[0] // JSON.stringify(data)
+        this.userProfile.data = data// JSON.stringify(data)
         // this.$store.commit('updateUserProfile',JSON.stringify(data));
-
+        if (this.userProfile.data.following === "undefined")
+        {
+        
+          this.following = 0; 
+        } else {
+          this.following = this.userProfile.data.following.length;
+        }
         // return data;
       } catch (error) {
         // console.log("I dont care");
       }
     },
+  async updateProfile() {
+      try {
+        const response = await fetch(`http://localhost:5000/profile`, {
+          method: 'POST',
+          // Adding body or contents to send
+          body: JSON.stringify(this.userProfile.data), // Adding headers to the request headers:
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        })
+        // console.log ("I am here");
+        const data = await response.json()
 
+        this.userProfile.data = data// JSON.stringify(data)
+        // this.$store.commit('updateUserProfile',JSON.stringify(data));
+        // return data;
+      } catch (error) {
+        // console.log("I dont care");
+      }
+    }
 
      }
     

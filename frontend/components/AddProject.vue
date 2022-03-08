0a9c2d81bbecdd82d3cb1282dc5a9a7235b4434d
Rafia7 <71458610+Rafia7@users.noreply.github.com>
2022-03-08 11:45:01 -0500

    Update AddProject.vue

--- frontend/components/AddProject.vue
+++ frontend/components/AddProject.vue
@@ -12,7 +12,7 @@
 export default {
   data() {
     return {
-      projects: {},
+      projects: [],
     }
   },
 
@@ -25,9 +25,12 @@ export default {
       const test = async function(){
         const data = await fetch('http://jsonplaceholder.typicode.com/posts');
         const newData= await data.json()
-        console.log(newData),
-        this.projects = newData;
-        console.log(this.projects)
+        console.log(newData);
+
+       const newData2 = this.projects.push(newData);
+       console.log(newData)
+        
+
         
       
       }

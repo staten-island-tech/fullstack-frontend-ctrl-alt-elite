0b2251330ce1806cd17df8bfe22572f1776cee68
cseifen123 <caroline.seifen22@sitechhs.com>
2022-03-08 11:45:19 -0500

    popup begin

--- frontend/components/UpdatePhotoButton.vue
+++ frontend/components/UpdatePhotoButton.vue
@@ -1,6 +1,11 @@
 <template>
-        <button class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</button> 
-         
+    <div>
+    <button class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</button> 
+        <div class="w-full h-full justify-center items-center absolute bg-transparent z-20 hidden">
+
+        </div>
+    </div>
+     
 </template>
 
 <script>

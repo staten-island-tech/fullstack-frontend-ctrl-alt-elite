0cbc9a1fccfd00c6f409954317211bdca3639bc5
cseifen123 <caroline.seifen22@sitechhs.com>
2022-03-09 12:28:42 -0500

    hvk

--- frontend/components/UpdatePhotoButton.vue
+++ frontend/components/UpdatePhotoButton.vue
@@ -2,7 +2,9 @@
     <div>
     <button class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</button> 
         <div class="w-full h-full justify-center items-center absolute bg-transparent z-20 hidden">
-
+            <div class="h-40 w-40 flex justify-center">
+                hi
+            </div>
         </div>
     </div>
      

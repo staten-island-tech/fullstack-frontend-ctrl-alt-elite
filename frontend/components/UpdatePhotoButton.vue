cfa9d522c159f82de7c2ed0135a50b23fad118b1
cseifen123 <caroline.seifen22@sitechhs.com>
2022-03-11 12:31:58 -0500

    click photo

--- frontend/components/UpdatePhotoButton.vue
+++ frontend/components/UpdatePhotoButton.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-    <div class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</div> 
-        <div class="w- h-100 justify-center items-center absolute bg-transparent z-20 hidden">
-          
+        <button @click="updatePhoto()" class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</button> 
+        <div class="w- h-100 justify-center items-center bg-blue">
+            <h1 class="text-white bg-blue-700 ">hello</h1>
         </div>
-    </div>
+     </div>
      
 </template>
 
 <script>
 
+export default {
+   data(){
+       return{
+           
+
+       }
+    }
+}
 </script>
+
+
+<style>
+</style>

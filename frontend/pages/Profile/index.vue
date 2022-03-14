e757c26bb64c8536db92f907c3c7bffa1c8f0ac5
cseifen123 <caroline.seifen22@sitechhs.com>
2022-03-14 12:27:06 -0400

    POPUP 1

--- frontend/pages/Profile/index.vue
+++ frontend/pages/Profile/index.vue
@@ -10,7 +10,10 @@
         <div class="flex flex-col-reverse justify-center gray-600 rounded-md items-center"> 
             <div class="flex flex-col justify-start  ">
               <div v-if="$auth.user.email === $store.state.otherIDInfo.email">
-                <UpdatePhotoButton/>
+                 <button  class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500 mt-2">Update Profile Photo</button> 
+                  <PopupPhoto>
+                    <h2>my popup</h2>
+                  </PopupPhoto>
               </div>
               <div v-else class="text-center" >
                 <FollowButton2 :followuserid=$store.state.otherIDInfo.email />
@@ -58,7 +61,8 @@ export default {
          userProfile: { data : 'abc'},
         following: 0,
         followers:0,
-        recentProjects :[]
+        recentProjects :[], 
+      
          }
       },
 

5d4833ab84e4fd21abdd1751b4bc75e32343a1fc
ZhangTerrence <71456929+TerrenceZhang74@users.noreply.github.com>
2022-05-12 12:25:47 -0400

    E

--- frontend/components/ProjectCard.vue
+++ frontend/components/ProjectCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="h-60 w-96 bg-l-bg-secondary dark:bg-d-bg-primary rounded border dark:border-slate border-light-gray" :class="{ light : !this.$store.state.darkMode }">
-    <div class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t" id="image" @click="otherProject"></div>
+    <iframe id="iframe" class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t">
+    </iframe>
     <div class="h-1/4 w-full flex flex-col justify-center">
       <h3 class="text-black dark:text-white w-full z-10 flex justify-center px-3 font-bold" @click="otherProject" :id="project.projects._id">{{ project.projects.project_title }}</h3>
       <div class="flex flex-row justify-between px-3">
@@ -16,6 +17,11 @@
 export default {
   props: {
     project: Object
+  },
+  mounted(){
+    document.getElementById("iframe").contentWindow.document.body.addEventListener("click", function(){
+      console.log("heello");
+    })
   },
   methods:{
     otherProject(){
@@ -37,9 +43,10 @@ export default {
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 }
 
-#image {
+#iframe {
   background-image:linear-gradient(rgb(0, 0, 0), rgba(0, 0, 0, 0.075)), url("./assets/welcoming-placeholder-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
 }
+
 </style>

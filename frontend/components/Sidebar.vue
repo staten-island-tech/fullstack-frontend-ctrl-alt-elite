71bf3b1609346df5d534e5824be3b01b4a6b0816
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-05-16 12:31:21 -0400

    -

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -2,16 +2,16 @@
   <div id="nav" class="h-screen">
       <font-awesome-icon v-if="!display" class="p-4 w-10 text-black dark:text-gray-100 text-xl" :icon="['fas', 'bars']"  @click="toggleVisible"/>
     <div :class="{ shown : display }" class="h-screen w-0 duration-100 bg-l-bg-primary dark:bg-d-bg-secondary absolute z-10">
-        <div v-if="display" class="h-full w-full border-r border-medium-gray dark:border-d-bg-accent">
+        <div v-if="display" class="h-full w-screen md:w-full border-r border-medium-gray dark:border-d-bg-accent bg-l-bg-primary dark:bg-d-bg-primary">
             <div class="flex flex-row justify-between">
                 <font-awesome-icon class="p-4 w-10 text-2xl text-black dark:text-gray-100" :icon="['fas', 'xmark']"  @click="toggleVisible"/>
                <img class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo.png">
             </div> 
-            <div class="flex flex-col align-center justify-center m-2 h-1/4 w-11/12 border-b border-t border-medium-gray dark:border-d-bg-accente">
+            <div class="flex flex-col align-center justify-center m-2 h-1/3 w-11/12 border-b border-t border-medium-gray dark:border-d-bg-accent">
                 <img class="rounded-full h-24 justify-self-center self-center m-1 " :src="info.profilePic">
                 <div class="text-black dark:text-light-gray flex items-center justify-center flex-col text-center">
                     <p class="font-bold ">{{info.name}}</p>
-                   <div class="text-sm flex flex-row justify-between m-2 w-2/3 text-black dark:text-white">
+                   <div class="text-sm flex flex-row justify-between m-2 w-1/2 md:w-2/3 text-black dark:text-white">
                        <p>Following  {{info.following}}</p> 
                         <p>Followers  {{info.followers}}</p>
                    </div>
@@ -26,7 +26,7 @@
                     <font-awesome-icon :icon="['fas', 'moon']" :class="{ selectedDark : this.$store.state.darkMode, light : !this.$store.state.darkMode }" class="text-lg text-slate"></font-awesome-icon>
                 </div>
             </div>
-            <div class="flex flex-col dark:text-white text-black h-1/2">
+            <div class="flex flex-col dark:text-white text-black h-1/2 ml-4 md:ml-0.5 text-base md:text-lg">
                 <div class="h-1/6">
                     <NuxtLink to="/Home" class="hover:bg-purple-300 hover:text-white bg-gradient-to-r hover:from-primary duration-75 h-full w-19/20 flex items-center pl-6 rounded-r-full">
                         <font-awesome-icon :icon="['fas', 'house']"></font-awesome-icon>
@@ -45,7 +45,7 @@
                         <p class="p-2">New Project</p>
                     </NuxtLink>
                 </div>
-                <button class="text-red-400 h-1/6 flex items-center pl-6" @click="logout">
+                <button class="text-base md:text-lg text-red-400 h-1/6 flex items-center pl-6" @click="logout">
                     <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']"></font-awesome-icon>
                     <LogoutButton class="text-left p-2"/>
                 </button>

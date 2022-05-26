a43c0abb3d7169f39ab115a931b1ec51cbd25fb1
jysleyin <69206877+jysleyin@users.noreply.github.com>
2022-05-26 12:07:30 -0400

    Revert "Merge branch 'julissa2' into development"
    This reverts commit 3bc682ad4e4f5cf85b6abfe99c54603d8525015c, reversing
    changes made to c3d1645ce029e1df42a9f46acd7bc376fc0ca2a7.

--- frontend/components/ProjectCard.vue
+++ frontend/components/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-60 w-96 bg-l-bg-secondary dark:bg-d-bg-primary rounded border dark:border-slate border-light-gray" :class="{ light : !this.$store.state.darkMode }">
-    <div class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t" id="image" @click="otherProject"></div>
+    <iframe id="iframe" class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t" :srcdoc="test"></iframe>
     <div class="h-1/4 w-full flex flex-col justify-center">
       <h3 class="text-black dark:text-white w-full z-10 flex justify-center px-3 font-bold" @click="otherProject" :id="project.projects._id">{{ project.projects.project_title }}</h3>
       <div class="flex flex-row justify-between px-3">
@@ -16,6 +16,21 @@
 export default {
   props: {
     project: Object
+  },
+  data(){
+    return{
+      test: `
+              <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>I See You Inspecting</title>
+                <style>${this.project.projects.published_code.css}</style>
+              </head>
+              ${this.project.projects.published_code.html}
+              <script>${this.project.projects.published_code.js}<\/script>
+            `
+    }
   },
   methods:{
     otherProject(){
@@ -38,9 +53,10 @@ export default {
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 }
 
-#image {
-  background-image:linear-gradient(rgb(0, 0, 0), rgba(0, 0, 0, 0.075)), url("./assets/welcoming-placeholder-bg.jpg");
+#iframe {
+  background-image:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url("./assets/welcoming-placeholder-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
 }
+
 </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-black h-full">
-    <WelcomingNavBar class="z-10 duration-75"/>   
-    <section id="welcoming-page"> 
-      <div class="flex flex-row items-center h-5/6" data-aos="fade-right">
-        <div class="m-0 ml-20 w-1/3">
-          <h1 class="text-white uppercase font-bold text-6xl">This is the welcoming page.</h1>
-          <p class="text-white text-xl my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi accusamus adipisci rem est perspiciatis odio. Aspernatur tenetur quis recusandae officiis quae quas aperiam quidem eos eveniet. Odit, quisquam ut!</p>
-          <SignupButton class="text-white border h-10 rounded-md w-1/3 ml-0">Sign up today!</SignupButton>
-         </div>  
+  <div class=" h-full ">
+    <WelcomingNavBar class="z-10 duration-75 md:text-lg text-xs"/>   
+    <section id="welcoming-page" class="md:p-24 p-0"> 
+      <div class="flex flex-row md:justify-start justify-center items-center h-5/6 mx-16 md:mx-0" data-aos="fade-right">
+        <div class="m-1 2xl:w-1/3 xl:w-1/3 lg:w-1/3 md:w-1/2 flex items-center justify-center flex-col md:items-start md:justify-around">
+          <h1 class="text-white uppercase font-bold 2xl:text-5xl xl:text-5xl lg:text-4xl md:text-4xl sm:text-3xl text-center md:text-left text-3xl">This is the welcoming page.</h1>
+          <p class="text-center md:text-left text-white 2xl:text-xl xl:text-lg lg:text-lg md:text-lg sm:text-base my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi accusamus adipisci rem est perspiciatis odio.</p>
+          <SignupButton class="text-white border h-10 w-2/3 sm:w-1/2 md:w-1/2 2xl:w-1/3 ml-0 2xl:text-xl xl:text-xl text-sm">Sign up today!</SignupButton>
+        </div>  
       </div>
       
     </section>

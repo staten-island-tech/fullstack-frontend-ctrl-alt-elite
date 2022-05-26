bce45edfd05f7073e3c4a8fa7bbbb7293d72e57b
cseifen123 <caroline.seifen22@sitechhs.com>
2022-05-26 12:04:16 -0400

    Revert "Merge branch 'development' of https://github.com/staten-island-tech/fullstack-frontend-ctrl-alt-elite into development"
    This reverts commit 5452a30dcafbe4c6611210277bedf7141b92c0ab, reversing
    changes made to f14c81715ba957e3737ea5d3189f74943d38d055.

--- frontend/components/ProjectCard.vue
+++ frontend/components/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-60 w-96 bg-l-bg-secondary dark:bg-d-bg-primary rounded border dark:border-slate border-light-gray" :class="{ light : !this.$store.state.darkMode }">
-    <div class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t" id="image" @click="otherProject"></div>
+    <iframe id="iframe" class="h-3/4 w-full bg-l-bg-secondary dark:bg-d-bg-primary flex justify-center items-center rounded-t" :srcdoc="test"></iframe>
     <div class="h-1/4 w-full flex flex-col justify-center">
       <h3 class="text-black dark:text-white w-full z-10 flex justify-center px-3 font-bold" @click="otherProject" :id="project.projects._id">{{ project.projects.project_title }}</h3>
       <div class="flex flex-row justify-between px-3">
@@ -17,9 +17,23 @@ export default {
   props: {
     project: Object
   },
+  data(){
+    return{
+      test: `
+              <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>I See You Inspecting</title>
+                <style>${this.project.projects.published_code.css}</style>
+              </head>
+              ${this.project.projects.published_code.html}
+              <script>${this.project.projects.published_code.js}<\/script>
+            `
+    }
+  },
   methods:{
     otherProject(){
-      console.log(this.project);
       const data = {
         projects: this.project.projects,
         projectName: this.project.projects.project_title
@@ -27,7 +41,7 @@ export default {
       this.$store.dispatch("viewOtherProject", data)
       this.$store.commit("newProject", false)
       this.$store.commit("isNotYourProject", true)
-      this.$router.push({path: "/Project"})
+      this.$router.push("Project")
     }
   }
 }
@@ -38,9 +52,10 @@ export default {
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 }
 
-#image {
-  background-image:linear-gradient(rgb(0, 0, 0), rgba(0, 0, 0, 0.075)), url("./assets/welcoming-placeholder-bg.jpg");
+#iframe {
+  background-image:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url("./assets/welcoming-placeholder-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
 }
+
 </style>

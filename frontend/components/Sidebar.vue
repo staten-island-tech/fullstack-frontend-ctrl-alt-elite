05427c6afc18a9856898fd92813494f419555d90
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-06-02 23:31:36 -0400

    ???

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav" class="h-screen relative">
-      <font-awesome-icon id="cursor" v-if="!display" class="navIcon text-black dark:text-gray-100" :icon="['fas', 'bars']"  @click="toggleVisible"/>
+      <font-awesome-icon id="cursor" v-if="!display" class=" navIcon text-black dark:text-gray-100" :icon="['fas', 'bars']"  @click="toggleVisible"/>
     <div :class="{ shown : display }" class="h-screen w-0 duration-100 bg-l-bg-primary dark:bg-d-bg-secondary absolute z-10">
         <div v-if="display" class="h-full w-screen md:w-full border-r border-medium-gray dark:border-d-bg-accent bg-l-bg-primary dark:bg-d-bg-primary">
             <div class="flex flex-row justify-between h-12">

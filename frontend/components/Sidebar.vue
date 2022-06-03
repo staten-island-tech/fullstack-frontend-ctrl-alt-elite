4e5d7ffeef0cd873b188a737dd3d226cbfdf020e
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-06-03 11:40:19 -0400

    Update Sidebar.vue

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -6,7 +6,7 @@
         <div v-if="display" class="h-full w-screen md:w-full border-r border-medium-gray dark:border-d-bg-accent bg-l-bg-primary dark:bg-d-bg-primary">
             <div class="flex flex-row justify-between h-12">
                 <!-- <font-awesome-icon class="navIcon text-black dark:text-gray-100" :icon="['fas', 'xmark']"  @click="toggleVisible"/> -->
-                <button class="navIcon text-black dark:text-gray-100"  @click="toggleVisible">10005</button>
+                <button class="navIcon text-black dark:text-gray-100"  @click="toggleVisible">U+2716</button>
                <img v-if="this.$store.state.darkMode" class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo.png">
                <img v-if="!this.$store.state.darkMode" class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo-light.png">
             </div> 
@@ -208,7 +208,7 @@ a.nuxt-link-exact-active {
 
 .navIcon {
   height: 20px;
-
+  padding: 1rem;
   cursor: pointer;
 }
 

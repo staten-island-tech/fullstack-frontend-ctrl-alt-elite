8c9fac48212e2cbd1397eca8b30552b1b174b95f
ZhangTerrence <terrencezhang77@gmail.com>
2022-06-02 20:09:10 -0400

    Fix bug

--- frontend/components/ProjectsNavBar.vue
+++ frontend/components/ProjectsNavBar.vue
@@ -1,32 +1,31 @@
 <template>
   <nav class="w-full h-1/10 flex flex-row justify-between items-center bg-d-bg-primary text-white">
-  <div class="w-1/2 flex flex-row py-2 items-center  border-1">
-    <NuxtLink to="/Home"><img class=" mx-4 h-8 sm:h-12 pl-1 sm:pl-2 items-center" src="../assets/codeverse-logo-shortened.png"></NuxtLink>
-    <div class="flex flex-col items-center ">
-      <div class="flex flex-row items-center">
-        <input v-model="title" placeholder="Title" type="text" class="h-1/12 w-32 ml-5 flex justify-center items-center test-sm sm:text-md bg-transparent rounded"> 
-        
+    <div class="w-1/2 flex flex-row py-2 items-center  border-1">
+      <button @click="toHome"><img class="mx-4 h-8 sm:h-12 pl-1 sm:pl-2 items-center" src="../assets/codeverse-logo-shortened.png"></button>
+      <div class="flex flex-col items-center">
+        <div class="flex flex-row items-center">
+          <font-awesome-icon icon="fa-solid fa-pen" class="px-3" />
+          <input id="title" v-model="title" placeholder="Title" type="text" class="h-1/12 w-32 ml-5 flex justify-center items-center test-sm sm:text-md bg-transparent rounded"> 
+        </div>
+        <a v-if="this.$store.state.otherUserProject === true" @click="viewProfile"><h1 class="h-1/12 w-5/6 sm:w-full flex justify-center items-center text-xs sm:text-sm bg-transparent hover:text-gray-400 text-black cursor-pointer">{{this.$store.state.otherUsername}}</h1></a>
       </div>
-      <NuxtLink to="/Profile"><input v-model="userProfile.data.name" placeholder="Username" type="text" :readonly="!ownProfile" class="h-1/12 w-5/6 sm:w-full flex justify-center items-center text-xs sm:text-sm bg-transparent hover:text-gray-400 text-black cursor-pointer" :class="{'focus:outline-none':!ownProfile}"  ></NuxtLink>
     </div>
-  </div>
- 
-    <div v-if="$store.state.otherUserProject === false" class="h-2/3 w-1/2 sm:1/3 md:w-2/5 xl:w-1/4 flex justify-around items-center">
-      <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="run">Run</button>
-      <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="run"><font-awesome-icon :icon="['fas', 'play']"/></button>
-      <button class="hidden md:flex bg-gray-500 hover:bg-gry-400 text-white py-2 px-4 rounded w-auto text-sm md:text-md" @click="save"> Save</button>
-       <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="save"><font-awesome-icon :icon="['fas', 'floppy-disk']"/></button>
-      <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="settings"> Settings</button>
-       <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="settings"><font-awesome-icon :icon="['fas', 'gear']"/></button>
-      <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md">Publish</button>
-      <img class="hidden md:flex basis-5 rounded-full h-10 justify-self-center self-center m-1 " :src="userProfile.data.profile_pic">
-    </div>
-    <div v-else-if="$store.state.otherUserProject === true">   
-      <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="run">Run</button>
-      <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="settings">Settings</button>
-      <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="copy">Copy</button>
-    </div>
-
+      <div class="h-2/3 w-1/2 sm:1/3 md:w-2/5 xl:w-1/4 flex justify-around items-center" v-if="this.$store.state.otherUserProject === false">
+        <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="run">Run</button>
+        <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="run"><font-awesome-icon :icon="['fas', 'play']"/></button>
+        <button class="hidden md:flex bg-gray-500 hover:bg-gry-400 text-white py-2 px-4 rounded w-auto text-sm md:text-md" @click="save"> Save</button>
+        <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="save"><font-awesome-icon :icon="['fas', 'floppy-disk']"/></button>
+        <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="settings"> Settings</button>
+        <button class="md:hidden flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md" @click="settings"><font-awesome-icon :icon="['fas', 'gear']"/></button>
+        <button class="hidden md:flex w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-md">Publish</button>
+        <img class="basis-5 rounded-full h-10 justify-self-center self-center m-1 " :src="userProfile.data.profile_pic" @click="viewProfile">
+      </div>
+      <div class="h-2/3 w-1/4 md:w-2/5 xl:w-1/4 flex justify-around items-center" v-else-if="this.$store.state.otherUserProject === true">   
+        <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="run">Run</button>
+        <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="settings">Settings</button>
+        <button class="w-auto bg-gray-500 hover:bg-gray-400 text-white  py-2 px-4 rounded text-sm md:text-base" @click="copy">Copy</button>
+        <img class="basis-5 rounded-full h-10 justify-self-center self-center m-1 " :src="userProfile.data.profile_pic" @click="viewProfile">
+      </div>
 </nav>
 </template>
 

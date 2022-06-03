dbafeee37f8fa3a50b759833f12b51a9c6d8a3e3
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-06-02 22:46:04 -0400

    Update NewProjectButton.vue

--- frontend/components/NewProjectButton.vue
+++ frontend/components/NewProjectButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center">
-        <button class="bg-d-primary text-white font-bold mx-4 cursor-pointer h-6 w-6 text-center flex items-center justify-center" @click="newProject" ><font-awesome-icon :icon="['fas', 'plus']" class="text-base rounded text-white"></font-awesome-icon></button>
+        <button class="bg-d-primary text-white font-bold mx-4 cursor-pointer h-6 w-6 text-center flex items-center justify-center" @click="newProject" ><font-awesome-icon :icon="['fas', 'plus']" class="icon text-base rounded text-white"></font-awesome-icon></button>
     </div>
 </template>
 <script>
@@ -25,5 +25,9 @@ export default {
 #NewProjectBtn:hover {
     transform: scale(1.2);
     transition: all .2s ease-in-out;
+}
+
+.icon {
+height: 15px
 }
 </style>

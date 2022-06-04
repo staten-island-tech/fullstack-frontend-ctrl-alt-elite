2a6d9eb0e07150a34210476d76722d6e32200277
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-06-04 13:04:14 -0400

    Update Sidebar.vue

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -2,23 +2,24 @@
   <div id="nav" class="h-screen relative">
       <font-awesome-icon id="cursor" v-if="!display" class="p-4 w-10 text-black dark:text-gray-100 text-xl" :icon="['fas', 'bars']"  @click="toggleVisible"/>
     <div :class="{ shown : display }" class="h-screen w-0 duration-100 bg-l-bg-primary dark:bg-d-bg-secondary absolute z-10">
-        <div v-if="display" class="h-full w-full border-r border-medium-gray dark:border-slate">
-            <div class="flex flex-row justify-between">
+        <div v-if="display" class="h-full w-screen md:w-full border-r border-medium-gray dark:border-d-bg-accent bg-l-bg-primary dark:bg-d-bg-primary">
+            <div class="flex flex-row justify-between h-12">
                 <font-awesome-icon id="cursor" class="p-4 w-10 text-2xl text-black dark:text-gray-100" :icon="['fas', 'xmark']"  @click="toggleVisible"/>
-               <img class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo.png">
+               <img v-if="this.$store.state.darkMode" class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo.png">
+               <img v-if="!this.$store.state.darkMode" class="flex h-16 mx-4 align-center justify-center" src="../assets/codeverse-logo-light.png">
             </div> 
-            <div class="flex flex-col align-center justify-center m-2 h-1/4 w-11/12 border-b border-t border-medium-gray dark:border-slate">
-                <img class="rounded-full h-24 w-24 justify-self-center self-center m-1 " :src="info.profilePic">
+            <div class="flex flex-col align-center justify-center m-2 h-1/3  w-11/12 border-b border-t border-medium-gray dark:border-slate">
+                <img class="rounded-full sm:w-20 md:w-24 w-16 justify-self-center self-center m-1 " :src="info.profilePic">
                 <div class="text-black dark:text-light-gray flex items-center justify-center flex-col text-center">
-                    <p class="font-bold ">{{info.name}}</p>
-                   <div class="text-sm flex flex-row justify-between m-2 w-2/3 text-black dark:text-white">
+                    <p class="font-bold text-xl md:text-base">{{info.name}}</p>
+                   <div class="text-sm flex flex-row justify-between m-2 w-1/2 md:w-2/3 text-black dark:text-white">
                        <p>Following  {{info.following}}</p> 
                         <p>Followers  {{info.followers}}</p>
                    </div>
-                   <p id="profile"  class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-primary hover:from-pink-500 hover:to-yellow-500 my-2 text-lg cursor-pointer" @click="getProfile(1)">View Profile</p>
+                   <button id="profile"  class="flex py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-primary hover:from-pink-500 hover:to-yellow-500 my-2 text-lg" @click="getProfile">View Profile</button>
                 </div>
             </div>
-            <div class="h-1/10 text-black dark:text-white flex items-center flex-col w-11/12 border-b border-medium-gray dark:border-slate m-2">
+            <div class="lg:h-1/10 h-1/8 text-black dark:text-white flex items-center flex-col w-11/12 border-b border-medium-gray dark:border-d-bg-accent m-2">
                 <p>Theme</p>
                 <div class="flex flex-row items-center">
                     <font-awesome-icon :icon="['fas', 'sun']" :class="{ selectedLight : !this.$store.state.darkMode }" class="text-lg text-mid-gray"></font-awesome-icon>
@@ -26,20 +27,18 @@
                     <font-awesome-icon :icon="['fas', 'moon']" :class="{ selectedDark : this.$store.state.darkMode, light : !this.$store.state.darkMode }" class="text-lg text-slate"></font-awesome-icon>
                 </div>
             </div>
-            <div class="flex flex-col dark:text-white text-black h-1/2">
+            <div class="flex flex-col dark:text-white text-black h-1/2 ml-4 md:ml-0.5 text-base md:text-lg">
                 <div class="h-1/6">
-                    <a class="hover:bg-gray-700 hover:text-white bg-gradient-to-r  duration-75 h-full w-19/20 flex items-center pl-6 rounded-r-full" @click="toHome">
+                    <NuxtLink to="/Home" class="hover:bg-gray-700 hover:text-white bg-gradient-to-r  duration-75 h-full w-19/20 flex items-center pl-6 rounded-r-full">
                         <font-awesome-icon :icon="['fas', 'house']"></font-awesome-icon>
                         <p class="p-2">Home</p>
-                    </a>
+                    </NuxtLink>
                 </div>
                 <div class="h-1/6">
-                  
-                    <div class="hover:bg-gray-700  hover:text-white bg-gradient-to-r duration-75 h-full w-full flex items-center pl-6 rounded-r-full" @click ="getProfile(3)" >
+                    <NuxtLink to="/Profile/Projects" class="hover:bg-gray-700  hover:text-white bg-gradient-to-r duration-75 h-full w-full flex items-center pl-6 rounded-r-full">
                         <font-awesome-icon :icon="['fas', 'pen']" ></font-awesome-icon>
                         <p class="p-2">View Projects</p>
-                    </div>
-                    
+                    </NuxtLink>
                 </div>
                 <div class="h-1/6">
                     <button class="hover:bg-gray-700  hover:text-white bg-gradient-to-r  duration-75 h-full w-full flex items-center pl-6 rounded rounded-r-full" @click="newProject">
@@ -47,7 +46,7 @@
                         <p class="p-2">New Project</p>
                     </button>
                 </div>
-                <button class="text-red-400 h-1/6 flex items-center pl-6" @click="logout">
+                <button class="text-base md:text-lg text-red-400 h-1/6 flex items-center pl-6" @click="logout">
                     <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']"></font-awesome-icon>
                     <LogoutButton class="text-left p-2"/>
                 </button>

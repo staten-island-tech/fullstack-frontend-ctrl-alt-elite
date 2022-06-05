a2bf2e2206a3235b079189c81ab2b32d03ff4700
IvyZ23 <71409315+IvyZ23@users.noreply.github.com>
2022-06-05 11:09:04 -0400

    Update ProjectsList.vue

--- frontend/components/ProjectsList.vue
+++ frontend/components/ProjectsList.vue
@@ -6,9 +6,9 @@
             <input 
             v-model="searchArgs" type="search" 
             class="form-control relative flex-auto block h-10 w-3/4  sm:w-full px-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" >
-            <!-- <button  class=" py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500  " @click="searchProjects" >Search</button> -->
+            <button  class=" py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500  " @click="searchProjects" >Search</button>
          </div>
-          <!-- <button  class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500  " @click="resetProjects" >Reset</button> -->
+          <button  class="py-2 px-4 rounded text-gray-900 font-bold bg-gradient-to-r from-purple-300 to-blue-700 hover:from-pink-500 hover:to-yellow-500  " @click="resetProjects" >Reset</button>
       </div>
     <div Class="flex flex-wrap relative flex-row justify-center px-4"  >
       <div v-for="(item,index) in projects" :key="item._ID">
